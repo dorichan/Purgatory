3786fd7c6babc508ba3f71d64799c75447643bc2
dorichan <dori.p.chan@gmail.com>
2015-02-04 23:11:13 -0800

    -initial commit

---
+++ Purgatory_v1.3/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour 
+{
+	public float speed = 10.0f;
+
+	private Transform player;
+
+	void Awake()
+	{
+		player = GameObject.FindWithTag("Player").transform;
+	}
+
+	void Update()
+	{
+		FollowPlayer();
+	}
+
+	void FollowPlayer()
+	{
+		float xMovement = Input.GetAxis ("Horizontal");
+//		float yMovement = Input.GetAxis ("Vertical");
+
+		Vector3 direction = player.transform.position - transform.position;
+		direction.z = -10.0f;
+
+		// Need a Lerp so that the camera doesn't stop so suddenly
+		if(xMovement > 0) {
+			transform.position += direction.normalized * speed * Time.deltaTime;
+		}
+		if(xMovement < 0) {
+			transform.position += direction.normalized * speed * Time.deltaTime;
+		}
+	}
+}
---
+++ Purgatory_v1.3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	private CharacterMemories characterMemories;
+	public CharacterDeath characterDeath;
+	public GameObject player;
+	public bool isDead;
+	public int memories;
+	public int deaths;
+
+	public int levelNum;
+	public int maxLevels = 6;
+	public string levelVersion;
+
+	void Start()
+	{
+		deaths = 1;		// Deaths start at one 
+		memories = 0;
+
+		DontDestroyOnLoad (GameObject.FindWithTag ("GameManager"));
+		levelNum = 1;
+	}
+
+	void Update()
+	{
+		if(isDead) {
+			if(characterMemories.goingHeaven) {
+				levelVersion = "heaven";
+			}
+			else if(characterDeath.goingHell) {
+				levelVersion = "hell";
+			}
+			StartCoroutine("LoadNewLevel");
+		}
+
+		player = GameObject.Find ("Player");
+		characterDeath = player.GetComponent<CharacterDeath> ();
+		characterMemories = player.GetComponent<CharacterMemories> ();
+	}
+
+	IEnumerator LoadNewLevel()
+	{
+		yield return new WaitForSeconds(3);
+
+		LoadLevel ();
+
+	}
+
+	void LoadLevel()
+	{
+		Application.LoadLevel ("level_" + levelNum + "_" + levelVersion);
+		if(isDead == true && levelVersion != null) {
+			isDead = false;
+			characterDeath.goingHell = false;
+			characterMemories.goingHeaven = false;
+			levelVersion = "";
+		}
+		StopCoroutine ("LoadNewLevel");
+	}
+}
